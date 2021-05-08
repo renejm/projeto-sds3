87e71b6d2275a94aa45b3b9c702386d81f5d3b09
Rene J. Martins <rene.j.martins@gmail.com>
2021-05-08 17:21:37 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,10 +1,33 @@
+import { Seller } from "./seller";
+
+export type Sale = {
+  id: number;
+  visited: number;
+  deals: number;
+  amount: number;
+  date: string;
+  seller: Seller;
+};
+
+export type SalePage = {
+  content?: Sale[];
+  first: boolean;
+  last: boolean;
+  totalPages: number;
+  size?: number;
+  totalElements: number;
+  number: number;
+  numberOfElements?: number;
+  empty?: boolean;
+};
+
 export type SaleSum = {
-    sellerName: string;
-    sum: number;
-}
+  sellerName: string;
+  sum: number;
+};
 
 export type SaleSuccess = {
-    sellerName: string;
-    visited: number;
-    deals: number;
-}
+  sellerName: string;
+  visited: number;
+  deals: number;
+};
